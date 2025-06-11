8f9f77c5c03227a4bb1dac0f0dbff8252c00a513
rbuj <robert.buj@gmail.com>
2025-06-11 12:03:16 +0200

    photoswipe: add img url in error message

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -7,4 +7,10 @@ const lightbox = new PhotoSwipeLightbox({
   showHideAnimationType: 'zoom',
   pswpModule: () => import('photoswipe/dist/photoswipe.esm.js')
 });
+lightbox.addFilter('contentErrorElement', (contentErrorElement, content) => {
+  const el = document.createElement('div');
+  el.className = 'pswp__error-msg';
+  el.innerHTML = `<a href="${content.data.src}" target="_blank">The image #${content.slide.index + 1}</a> cannot be loaded</a>`;
+  return el;
+});
 lightbox.init();
